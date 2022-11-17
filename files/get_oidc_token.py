88c07c9cf4af5d01bec2eafd2a0d4c40faa0ca3a
Jason Bisson <jasonbisson@google.com>
2022-11-17 11:46:41 -0500

    Okta python script update

--- files/get_oidc_token.py
+++ files/get_oidc_token.py
@@ -7,17 +7,21 @@ from google.auth import identity_pool
 import requests
 import http.client
 from pprint import pprint
-import requests
-import json
+import base64
 
 def get_okta_token():
+    okta_az_server = os.environ["OKTA_AZ_SERVER"]
+    client_id = os.environ["CLIENT_ID"]
+    client_secret = os.environ["CLIENT_SECRET"]
+    encodedData = base64.b64encode(bytes(f"{client_id}:{client_secret}", "ISO-8859-1")).decode("ascii")
+    
     cookies = {
         'JSESSIONID': 'C4178E6BB95095E2E6652D69CC3B716A',
     }
 
     headers = {
         'Accept': '*/*',
-        'Authorization': 'Basic <Base64enc clientid:clientsecret>',
+        'Authorization': 'Basic %s' % {encodedData},
         'Cache-Control': 'no-cache',
         'Content-Type': 'application/x-www-form-urlencoded',
     }
@@ -26,7 +30,7 @@ def get_okta_token():
         'grant_type': 'client_credentials'
     }
 
-    response = requests.post('<okta_az_server>/v1/token',
+    response = requests.post(okta_az_server,
                              headers=headers, cookies=cookies, data=data)
     response.raise_for_status()
     print("Creating Okta token file")
@@ -34,17 +38,6 @@ def get_okta_token():
     with open('/tmp/okta-token.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-
 if __name__ == '__main__':
     print("Running main")
     get_okta_token()
-    # For service account credentials
-    file = open('client-config.json')
-    json_config_info = json.loads(file.read())
-    scopes = ['https://www.googleapis.com/auth/devstorage.full_control',
-              'https://www.googleapis.com/auth/devstorage.read_only', 'https://www.googleapis.com/auth/devstorage.read_write']
-    #credentials  = google.auth.default(scopes=scopes)
-    credentials = identity_pool.Credentials.from_info(json_config_info)
-    scoped_credentials = credentials.with_scopes(scopes)
-    print("Removing Okta token file")
-    os.remove('/tmp/okta-token.json')
